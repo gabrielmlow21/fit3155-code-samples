e1419f891f768a9b9f6f9b0801ede69b6390dbd4
gabriel <lowgabriel213@gmail.com>
2022-08-16 11:03:57 +0800

    added select_edge method

--- week_3/suffix_tree.py
+++ week_3/suffix_tree.py
@@ -1,6 +1,7 @@
 class SuffixTree:
     terminal = '$'
-    active_point = ActivePoint()
+    root = None
+    active_point = None
     input = []
     remaining = 0
     end = None
@@ -19,20 +20,36 @@ class SuffixTree:
         root = SuffixNode(1, End(0))
         root.index = -1
         self.end = End(-1)
+        self.active_point = ActivePoint(root)
         for i in range(len(self.input)):
             self.start_phase(i)
         self.set_index(root, 0, len(input))
     
     def start_phase(self, i):
         last_internal_node = None
-        self.end.end = self.end.end + 1
-        self.remaining = self.remaining + 1
+        self.end.end += 1
+        self.remaining += 1
         while self.remaining > 0:
-            if 
+            if self.active_point.active_length == 0:
+                if self.active_point.active_node.children[self.input[i]] != None:
+                    self.active_point.active_edge = self.active_point.active_node.children[input[i]].start
+                    self.active_point.active_length += 1
+                    break
+                else:
+                    self.root.children[input[i]] = SuffixNode(i, self.end)
+                    self.remaining -= 1
+            else:
+                c = self.get_next_character(i)
+    
+    def get_next_character(self, i):
+        edge = self.select_edge()
+    
+    def select_edge(self):
+        return self.active_point.active_node.children[self.input[self.active_point.active_edge]]
 
 
 
-class SuffixNode():
+class SuffixNode:
     max_characters = 256
     children = [None] * max_characters
     suffix_link = None
